107703e016f90924a6efb1c7438e66b1ba97abec
arbass <13ar.bass@gmail.com>
2024-05-31 15:19:51 +0300

    updated: colored

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { cityDetector_func } from '$utils/city-detector';
 import { expParams_func } from '$utils/exp-catalog-params';
 import { formSelectors_func } from '$utils/form-selectors';
+import { coloredSlider_func } from '$utils/sliders-colors';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   cityDetector_func();
   formSelectors_func();
   expParams_func();
+  coloredSlider_func();
 });
---
+++ src/utils/sliders-colors.ts
@@ -0,0 +1,17 @@
+export const coloredSlider_func = () => {
+  const coloredSlider_el = document.querySelectorAll('[colored-slider]');
+
+  if (coloredSlider_el.length) {
+    coloredSlider_el.forEach((swiperColored) => {
+      const currentColorPattern = swiperColored.getAttribute('colored-slider');
+      const colors = currentColorPattern.split(',').map((color) => color.trim());
+      const allSwiperSlides = swiperColored.querySelectorAll('.swiper-slide');
+
+      allSwiperSlides.forEach((slide, index) => {
+        const colorIndex = index % colors.length;
+        console.log(`Applying color ${colors[colorIndex]} to slide ${index}`);
+        slide.style.backgroundColor = colors[colorIndex];
+      });
+    });
+  }
+};
