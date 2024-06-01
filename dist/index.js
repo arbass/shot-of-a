02be6edb2856abab7a32fd0b0f107fe9eb0dd574
arbass <13ar.bass@gmail.com>
2024-06-01 23:40:29 +0300

    updated: anc

--- dist/index.js
+++ dist/index.js
@@ -275,10 +275,26 @@
 
   // src/utils/exp-slider-link-creator.ts
   var expSliderLinkCreator_func = () => {
-    const expSliderLinkCreator_el = document.querySelectorAll("[colored-slider]");
+    const expSliderLinkCreator_el = document.querySelectorAll("[abs-link-for-append]");
     if (expSliderLinkCreator_el.length) {
-      expSliderLinkCreator_el.forEach((linkCreator_el) => {
-        console.log("test");
+      const abs_link_individual = document.querySelector("[abs-link-exp-individual]");
+      const abs_link_group = document.querySelector("[abs-link-exp-group]");
+      const all_sliders_individual = document.querySelectorAll("[exp-slider-individual]");
+      const all_sliders_group = document.querySelectorAll("[exp-slider-group]");
+      const city_current = document.querySelector("[page-city-current]").getAttribute("page-city-current");
+      all_sliders_individual.forEach((slider_individual) => {
+        const clone_abs_link = abs_link_individual.cloneNode(true);
+        clone_abs_link.classList.remove("hide");
+        const link_updated = clone_abs_link.getAttribute("href") + "#" + slider_individual.getAttribute("exp-name");
+        clone_abs_link.setAttribute("href", link_updated);
+        slider_individual.appendChild(clone_abs_link);
+      });
+      all_sliders_group.forEach((slider_group) => {
+        const clone_abs_link = abs_link_group.cloneNode(true);
+        clone_abs_link.classList.remove("hide");
+        const link_updated = clone_abs_link.getAttribute("href") + "#" + slider_group.getAttribute("exp-name");
+        clone_abs_link.setAttribute("href", link_updated);
+        slider_group.appendChild(clone_abs_link);
       });
     }
   };
