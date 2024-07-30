26aac76db7df48e69cbf7c5d5e0bd80b12b5a9ca
arbass <13ar.bass@gmail.com>
2024-07-30 20:31:08 +0300

    updated

--- src/utils/exp-selector.ts
+++ src/utils/exp-selector.ts
@@ -3,7 +3,40 @@ export const expSelector_func = () => {
 
   if (expSelector_el.length) {
     expSelector_el.forEach((bodyElement) => {
-      //
+      const allDropDowns = document.querySelectorAll('[exp-variants-dropdown]');
+      allDropDowns.forEach((allDropDown_el) => {
+        const currentDropdownMenu = allDropDown_el.querySelectorAll(
+          '[exp-dropdown-button-list-new]'
+        );
+        currentDropdownMenu.forEach((currentDropdownMenu_el) => {
+          const allSvgInside = currentDropdownMenu_el.querySelectorAll('svg');
+          allSvgInside.forEach((svg) => {
+            svg.classList.add('hide');
+          });
+          //
+          const allExpButtons = currentDropdownMenu_el.querySelectorAll('[exp-city-dropdown]');
+          allExpButtons.forEach((el_allExpButtons) => {
+            const currentListElement = el_allExpButtons.querySelector('[exp-city-dropdown-list]');
+            currentListElement.classList.add('hide');
+
+            //
+            el_allExpButtons.addEventListener('click', function () {
+              if (localStorage.getItem('savedCity')) {
+                const detectedCity = localStorage.getItem('savedCity');
+                const allIncludedLinks = el_allExpButtons.querySelectorAll('a');
+                allIncludedLinks.forEach((link) => {
+                  const currentLinkCity = link.getAttribute('exp-city-dropdown-city-slug');
+                  if (currentLinkCity === detectedCity) {
+                    window.location.href = link.href;
+                  }
+                });
+              } else {
+                alert('Please select the required city from the website menu.');
+              }
+            });
+          });
+        });
+      });
     });
   }
 };
